17bd6044624bfeb318121b4ea67b908018824879
Pavithra8805 <pavithra8805@gmail.com>
2024-02-10 11:47:26 +0000

    Protips

---
+++ pro-tips/src/Pages/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div
+            style={{ textAlign: "center", marginTop: "20px", fontSize: "24px" }}
+        >
+            <h1>Contact</h1>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ pro-tips/src/Pages/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../../Components/Form/Form";
+import "./Register.css";
+
+const Register = () => {
+    return (
+        <div className="register-container">
+            <Form />
+        </div>
+    );
+};
+
+export default Register;
